4afb57766ac66355a63e68c983ec1e857ef8306c
jashanbhullar <jsbhullar96@gmail.com>
2019-05-26 00:25:26 +0530

    - Lectues are prefixed with order now

--- downloadVideo.js
+++ downloadVideo.js
@@ -4,13 +4,26 @@ var interval = window.setInterval(() => {
   url = document.getElementsByTagName("video")[0];
   console.log("timer running", url);
   if (url.src) {
+    var arr = [
+      ...document.querySelectorAll(".course-toc__item.video-item.ember-view")
+    ];
+
+    const activeElement = document.querySelector(
+      '.active[data-control-name="course_video_route"]  .course-toc__item-content'
+    );
+
+    const toFind = activeElement.parentElement.parentElement.parentElement;
+
+    const index = arr.findIndex(el => el === toFind) + 1;
+
     chrome.runtime.sendMessage({
       action: "download",
       url: url.src,
       name:
-        (document.querySelector(
-          '.active[data-control-name="course_video_route"]  .course-toc__item-content'
-        ).childNodes[0].nodeValue || "404, Name not found") + ".mp4"
+        index.toString() +
+        ". " +
+        (activeElement.childNodes[0].nodeValue || "404, Name not found") +
+        ".mp4"
     });
     window.close();
     window.clearInterval(interval);
